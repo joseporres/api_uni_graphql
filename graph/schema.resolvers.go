5366ea245294322bef32aa6ee85834d4655b1e35
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-08-03 14:39:27 -0500

    feat: implemented deleteStudent & getStudents

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -7,88 +7,95 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/joseporres/api_uni_graphql/graph/models"
 	"github.com/joseporres/api_uni_graphql/graph/generated"
 	"github.com/joseporres/api_uni_graphql/graph/model"
+	"github.com/joseporres/api_uni_graphql/graph/models"
 )
 
 // CreateStudent is the resolver for the createStudent field.
 func (r *mutationResolver) CreateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
 	db := models.FetchConnection()
-    defer db.Close()
+	defer db.Close()
 
 	student := model.Student{Nombre: nombre, Dni: dni, Direccion: direccion, FechaNacimiento: fechaNacimiento}
 
 	db.Create(&student)
 
-	return &student,nil;
+	return &student, nil
 
 }
 
 // CreateCourse is the resolver for the createCourse field.
 func (r *mutationResolver) CreateCourse(ctx context.Context, nombre string, descripcion *string, temas *string) (*model.Course, error) {
 	db := models.FetchConnection()
-    defer db.Close()
+	defer db.Close()
 
 	course := model.Course{Nombre: nombre, Descripcion: descripcion, Temas: temas}
 
 	db.Create(&course)
-	
-	return &course,nil;
+
+	return &course, nil
 }
 
 // CreateRecord is the resolver for the createRecord field.
 func (r *mutationResolver) CreateRecord(ctx context.Context, student string, course string, startdate *string, finishdate *string) (*model.Record, error) {
 	db := models.FetchConnection()
-    defer db.Close()
+	defer db.Close()
 
 	record := model.Record{Student: student, Course: course, Startdate: startdate, Finishdate: finishdate}
 
 	db.Create(&record)
-	
-	return &record,nil;
+
+	return &record, nil
 }
 
 // UpdateStudent is the resolver for the updateStudent field.
 func (r *mutationResolver) UpdateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
 	db := models.FetchConnection()
-    defer db.Close()
-	
+	defer db.Close()
+
 	student := model.Student{Dni: dni}
 
 	db.Model(&student).Where("dni = ?", dni).Update(&model.Student{Nombre: nombre, Direccion: direccion, FechaNacimiento: fechaNacimiento})
 
-	return &student,nil;
+	return &student, nil
 
 }
 
 // UpdateCourse is the resolver for the updateCourse field.
 func (r *mutationResolver) UpdateCourse(ctx context.Context, nombre string, descripcion *string, temas *string) (*model.Course, error) {
 	db := models.FetchConnection()
-    defer db.Close()
-	
+	defer db.Close()
+
 	course := model.Course{Nombre: nombre}
 
 	db.Model(&course).Where("nombre = ?", nombre).Update(&model.Course{Descripcion: descripcion, Temas: temas})
 
-	return &course,nil;
+	return &course, nil
 }
 
 // UpdateRecord is the resolver for the updateRecord field.
 func (r *mutationResolver) UpdateRecord(ctx context.Context, student string, course string, startdate *string, finishdate *string) (*model.Record, error) {
 	db := models.FetchConnection()
-    defer db.Close()
-	
+	defer db.Close()
+
 	record := model.Record{Student: student, Course: course}
 
-	db.Model(&record).Where("student = ? AND course = ?", student,course).Update(&model.Record{Startdate: startdate, Finishdate: finishdate})
+	db.Model(&record).Where("student = ? AND course = ?", student, course).Update(&model.Record{Startdate: startdate, Finishdate: finishdate})
 
-	return &record,nil;
+	return &record, nil
 }
 
 // DeleteStudent is the resolver for the deleteStudent field.
 func (r *mutationResolver) DeleteStudent(ctx context.Context, dni string) (*model.Student, error) {
-	panic(fmt.Errorf("not implemented"))
+	db := models.FetchConnection()
+	defer db.Close()
+
+	student := model.Student{Dni: dni}
+
+	db.Where("dni = ?", dni).First(&student).Delete(&student)
+
+	return &student, nil
 }
 
 // DeleteCourse is the resolver for the deleteCourse field.
@@ -103,7 +110,14 @@ func (r *mutationResolver) DeleteRecord(ctx context.Context, student string, cou
 
 // GetStudents is the resolver for the getStudents field.
 func (r *queryResolver) GetStudents(ctx context.Context) ([]*model.Student, error) {
-	panic(fmt.Errorf("not implemented"))
+	db := models.FetchConnection()
+
+	defer db.Close()
+
+	var students []*model.Student
+	db.Find(&students)
+
+	return students, nil
 }
 
 // GetCourses is the resolver for the getCourses field.
@@ -137,6 +151,5 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type mutationResolver struct{ *Resolver}
+type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
